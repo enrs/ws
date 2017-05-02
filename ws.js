9f7fbf14b2278ff710c28124a59db75d4dbb0933
enrs <enrs@cumt.edu.cn>
2017-05-02 20:17:07 +0800

    Update ws.js

--- ws.js
+++ ws.js
@@ -1,7 +1,8 @@
 /*
 Yance
 weixin:justforto
-Version:1.0.1
+Version:1.0.2
+date:20170420
 */
 (function (root, factory) {
 	if (typeof exports === 'object' && typeof module === 'object')
@@ -14,13 +15,13 @@ Version:1.0.1
 		root["EventBus"] = factory();
 })(this, function () {
 
-	var EventBusClass = {},__get=false;
+	var EventBusClass = {}, __get = false;
 	EventBusClass = function () {
 		this.listeners = {};
 	};
 	EventBusClass.prototype = {
 		addEventListener: function (type, callback, scope) {
-			if(__get&&type=="__get")return;
+			if (__get && type == "__get") return;
 			var args = [];
 			var numOfArgs = arguments.length;
 			for (var i = 0; i < numOfArgs; i++) {
@@ -63,7 +64,7 @@ Version:1.0.1
 			}
 			return false;
 		},
-		dispatch: function (type, target,data) {
+		dispatch: function (type, target, data) {
 			//var event = {
 			//	type: type,
 			//	target: target
@@ -100,8 +101,26 @@ Version:1.0.1
 			return str;
 		}
 	};
+	var runError = m => {
+		wx.showModal({
+			title: '提示 ' + m,
+			content: '当前微信版本过低，无法使用该功能，请升级到最新微信版本后重试。',
+			//success: ()=>runError(m)
+		})
+
+	}
 
 	var ws = {}
+	ws.run = function (m) {
+		if (typeof wx[m]!='function') return runError(m);
+		var args = [];
+		var numOfArgs = arguments.length;
+		for (var i = 0; i < numOfArgs; i++) {
+			args.push(arguments[i]);
+		};
+		wx[m].apply(wx,args.splice(1, args.length - 1))
+	}
+
 	ws.showModal = function (config, fun) {
 		var def = {
 			title: '提示',
@@ -114,7 +133,7 @@ Version:1.0.1
 			complete: fun || function (res) { },//接口调用结束的回调（调用成功、失败都会执行）
 		}
 		typeof config == "string" ? def.content = config : Object.assign(def, config)
-		wx.showModal(def)
+		ws.run("showModal", def)
 	}
 	ws.showToast = function (config, fun) {
 		var def = {
@@ -127,7 +146,7 @@ Version:1.0.1
 			complete: fun || function (res) { },//接口调用结束的回调（调用成功、失败都会执行）
 		}
 		typeof config == "string" ? def.title = config : Object.assign(def, config)
-		wx.showToast(def)
+		ws.run("showToast", def)
 	}
 	ws.showLoading = function (config, fun) {
 		var def = {
@@ -138,51 +157,47 @@ Version:1.0.1
 			complete: fun || function (res) { },//接口调用结束的回调（调用成功、失败都会执行）
 		}
 		typeof config == "string" ? def.title = config : Object.assign(def, config)
-		wx.showLoading(def)
+		ws.run("showLoading", def)
 	}
-	ws.hideLoading = wx.hideLoading;
-
+	ws.hideLoading =()=> ws.run("hideLoading");
+	ws.set = function (k, v) { ws.run("setStorageSync", k, v) }
+	ws.get = function (k) { return ws.run("getStorageSync", k) }
 	var eventbus = new EventBusClass();
-	var wsSend = function (data) {
+	var wsSend = function (data, fun) {
+		if (typeof fun != "function") fun = function () { };
 		if (!data.type) return;
 		if (wsStatus) {
-			wx.sendSocketMessage({
-				data: '|' + data.type + '|' + JSON.stringify(data)
+			ws.run("sendSocketMessage", {
+				data: JSON.stringify(data),
+				success: () => fun(true),
+				fail: () => fun(false)
 			})
 		}
 		else {
-			if (wsReCount > 2) {
-				wsReCount = 0;
-				ws.showModal("请重新连接服务", function () {
-					initSocket(function () { wsSend(data) })
-				});
-			} else {
-				wsReCount++;
-				initSocket(function () { wsSend(data) })
-			}
+			initSocket(() => wsSend(data, fun))
 		}
 	}
-	var wsStatus = false, getDataID = 0, msgQueue = {}, wsReCount = 0;
-	ws.listen =function(type, fun, that){eventbus.addEventListener.apply(eventbus,arguments)}
+	var wsStatus = false, getDataID = 0, msgQueue = {};
+	ws.listen = function (type, fun, that) {
+		eventbus.addEventListener.apply(eventbus, arguments)
+	}
 	ws.send = function (type, data, fun, that) {
 		if (typeof type != "string") return;
 		if (typeof fun != "function") fun = function () { };
 		getDataID++;
 		msgQueue[getDataID] = { id: getDataID, callback: fun, that: that };
 		var init = {
-			type: type, data: data, id: getDataID
+			type: type, data: data, id: getDataID,uid:userInfo&&userInfo.uid,
 		};
-		wsTimeOut(getDataID);
-		wsSend(init);
+		wsSend(init, status => { wsTimeOut(getDataID, status) });
 	}
-	var wsTimeOut = function (id) {
+	var wsTimeOut = function (id, status) {
+		if (!id || !msgQueue[id]) return;
 		var timeid = setTimeout(function () {
-			if (id && msgQueue[id]) {
-				var c = msgQueue[id];
-				delete msgQueue[id];
-				c.callback.apply(c.that, [{ status: false, msg: '连接超时' }]);
-			}
-		}, 5000)
+			var c = msgQueue[id];
+			delete msgQueue[id];
+			c.callback.apply(c.that, [{ status: false, msg: status ? '连接超时' : "发送失败" }]);
+		}, status ? 5000 : 0)
 		msgQueue[id].timeid = timeid
 	}
 	eventbus.addEventListener("__get", function (data) {
@@ -194,41 +209,30 @@ Version:1.0.1
 			c.callback.apply(c.that, [data]);
 		}
 	})
-	__get=true;
-	var initSocket = function (fn) {
+	__get = true;
+	var initSocket = function (fun) {
 		ws.showLoading('连接中...');
-		if (typeof fn != "function") fn = function () { };
-		var fun = function () { ws.hideLoading(); wsReCount = 0; fn(); };
-		wx.connectSocket({
+		if (typeof fun != "function") fun = function () { };
+		ws.run("connectSocket", {
 			url: "wss://lrs.peernat.com/websocket/"
 		});
 		wx.onSocketOpen(function (res) {
-			wsStatus = true;
-			login(userInfo,fun);
+			wsStatus = true; console.log('连接上了');
+			ws.hideLoading(); fun();
 		});
-		var login=function(data,fun){
-			ws.showLoading('连接中...');
-			ws.send('login', data, function (d) {
-				ws.hideLoading();
-				if(d.status)
-				{
-				fun(); wsload();
-				}
-				else
-				{
-					ws.showModal(d.msg||"错误",function(){login(data,fun)});
-				}
-			})
-		}
 		wx.onSocketMessage(function (res) {
-			var data = JSON.parse(res.data);
+			var data;
+			try { data = JSON.parse(res.data); }
+			catch (e) { console.error(e.message, res); return }
+			console.log('来消息', data);
 			var type = data.type;
 			if (data.id && msgQueue[data.id]) {
 				eventbus.dispatch('__get', this, data);
+			} else {
+				eventbus.dispatch(type, this, data);
 			}
-			eventbus.dispatch(type, this, data);
 		});
-		wx.onSocketError(function (res) {
+		wx.onSocketError(function (res,a) {
 			ws.hideLoading();
 			if (res.message == 'websocket is connected') {
 				wsStatus = true;
@@ -236,42 +240,76 @@ Version:1.0.1
 			}
 			else {
 				wsStatus = false;
-				ws.showModal("服务器无法连接", function () { initSocket(fun) });
+				ws.showModal("服务器无法连接 "+res.errMsg, function () { initSocket(fun) });
 			}
 		});
 		wx.onSocketClose(function (res) { wsStatus = false; ws.hideLoading() });
 	}
 
-	var userInfo = null;
-	ws.getUserInfo = function (fun,that) {
+	var userInfo = null, wxUserInfo = null, openid = null;
+	var _getUserInfo = function (fun, that) {
 		if (typeof fun != "function") fun = function () { };
 		if (userInfo) {
-			fun.apply(that,[userInfo.userInfo,userInfo])
-		} else {
-			//调用登录接口
-			ws.showLoading()
-			wx.login({
-				success: function (a, b) {
-					wx.getUserInfo({
-						success: function (res) {
-							userInfo = res
-							ws.hideLoading();
-							fun.apply(that,[userInfo.userInfo,userInfo])
-						},
-						fail: function () {
-							ws.hideLoading();
-							ws.showModal("请重新确认授权", ws.getUserInfo);
-						}
-					})
-				}
-			})
+			fun.apply(that, [userInfo]);
+			return
 		}
-	}
+		userInfo = ws.get('userInfo');
+		if (userInfo) {
+			fun.apply(that, [userInfo]);
+			return
+		}
+		if (wxUserInfo) {
+			login(() => fun.apply(that, [userInfo]));
+			return
+		}
+		//调用登录接口
+		ws.showLoading()
+		wx.login({
+			success: function (wxcode) {
+				wx.getUserInfo({
+					withCredentials:false,
+					success: function (res) {
+						wxUserInfo = res;
+						wxUserInfo.userInfo.code = wxcode.code;
+						wxUserInfo.userInfo.model = wx.getSystemInfoSync().model;
+						ws.hideLoading();
+						login(() => fun.apply(that, [userInfo]));
+					},
+					fail: function () {
+						ws.hideLoading();
+						ws.showModal("请重新确认授权", () => _getUserInfo(fun, that));
+					}
+				})
+			}
+		})
 
-	ws.getUserInfo(initSocket);
+	}
+	ws.getUserInfo = (fun, that) => { ws.onload(() => _getUserInfo(fun, that)) }
+	_getUserInfo();//初始化
+	var login = function (fun) {
+		ws.showLoading('连接中...');
+		var user = {};
+		if (openid)
+		{ user.openid = openid }
+		else {
+			user = wxUserInfo.userInfo;
+		}
+		ws.send('login', user, function (d) {
+			if (typeof d.data == 'object')
+				openid = d.data.openid;
+			ws.hideLoading();
+			if (d.status) {
+				userInfo = d.data;
+				fun(); wsload();
+			}
+			else {
+				ws.showModal(d.msg || "错误", function () { login(fun) });
+			}
+		})
+	}
 	var _onload = [], _loadstatus = false;
 	var wsload = function () {
-		_loadstatus=true;
+		_loadstatus = true;
 		while (_onload.length > 0) {
 			var d = _onload.shift();
 			d.fun.apply(d.that, d.args)
